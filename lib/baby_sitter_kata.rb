ab8e194e0ae7b5395d59c4856e286f4fc0ea82b5
Reyes Montemayor <sjpete1234@hotmail.com>
2021-08-21 12:19:18 -0400

    Fixing the hourly wage to convert time to number

--- lib/baby_sitter_kata.rb
+++ lib/baby_sitter_kata.rb
@@ -14,7 +14,6 @@ def shift(start_shift, in_bed_time, end_shift)
   time_and_pay[:start] = start_time
   time_and_pay[:bed_time] = bed_time
   time_and_pay[:end] = end_time
-  time_and_pay[:pay] = calculate_hourly_rate(start_time, bed_time, end_time)
   if start_time.hour <= 16 || end_time.hour >= 4
     time_and_pay[:error] = "Please check your start time #{start_time.hour} and end time #{end_time.hour} cannot be earlier than 17 or leave earlier than 4"
   else
@@ -29,12 +28,12 @@ def calculate_hourly_rate(start_shift, in_bed_time, end_shift)
   start_time = start_shift
   bed_time = in_bed_time
   end_time = end_shift
-  total_amount = ((in_bed_time - start_shift).to_i * 12)
+  total_amount = (((in_bed_time - start_shift) * 24) * 12)
   if end_shift > midnight
-    total_amount += ((midnight - bed_time).to_i * 8)
-    total_amount += ((end_shift - midnight).to_i * 16)
+    total_amount += (((midnight - bed_time) * 24) * 8)
+    total_amount += (((end_shift - midnight) * 24) * 16)
   else
-    total_amount += ((midnight - bed_time).to_i * 8)
+    total_amount += (((midnight - bed_time) * 24) * 8)
   end
   total_amount = total_amount.round(0)
   return total_amount
--- spec/baby_sitter_kata_spec.rb
+++ spec/baby_sitter_kata_spec.rb
@@ -42,6 +42,11 @@ describe "Baby Sitter program" do
     expect(response[:error]).to eq("Please check your start time, bedtime, and end time. Invalid date time format was entered.")
   end
 
+  it "Baby sitter calculate hourly wage?" do
+    response = shift("2021-08-21 17:00:00", "2021-08-21 21:00:00", "2021-08-22 04:00:00")
+    expect(response[:error]).to eq("Please check your start time 17 and end time 5 cannot be earlier than 17 or leave earlier than 4")
+  end
+
   # it "Does the baby sitter have a end time?" do
   #   expect().to eq
   # end
