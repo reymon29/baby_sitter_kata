658b6c578503285e947fc845832a7f4c1d169fa1
Reyes Montemayor <sjpete1234@hotmail.com>
2021-08-21 14:01:10 -0400

    Fix logic if end shift is within the same date adding a elsif and separate hourly check in the if statement

--- lib/baby_sitter_kata.rb
+++ lib/baby_sitter_kata.rb
@@ -14,12 +14,16 @@ def shift(start_shift, in_bed_time, end_shift)
   time_and_pay[:start] = start_time
   time_and_pay[:bed_time] = bed_time
   time_and_pay[:end] = end_time
-  if start_time.hour <= 16 || end_time.hour > 4
-    time_and_pay[:error] = "Please check your start time #{start_time.hour} and end time #{end_time.hour} cannot be earlier than 17 or leave earlier than 4"
-  else
+  if start_time.hour <= 16
+    time_and_pay[:error] = "Please check your start time #{start_time.hour} and end time #{end_time.hour} cannot be earlier than 17 or leave later than 4"
+  elsif Date.parse("#{start_time}") == Date.parse("#{end_time}")
     time_and_pay[:pay] = calculate_hourly_rate(start_time, bed_time, end_time)
+  elsif end_time.hour > 4
+    time_and_pay[:error] = "Please check your start time #{start_time.hour} and end time #{end_time.hour} cannot be earlier than 17 or leave later than 4"
+  else
+   time_and_pay[:pay] = calculate_hourly_rate(start_time, bed_time, end_time)
   end
-  return time_and_pay
+ time_and_pay
 end
 
 def calculate_hourly_rate(start_shift, in_bed_time, end_shift)
@@ -33,7 +37,7 @@ def calculate_hourly_rate(start_shift, in_bed_time, end_shift)
     total_amount += (((midnight - bed_time) / 3600) * 8)
     total_amount += (((end_shift - midnight) / 3600) * 16)
   else
-    total_amount += (((end_shift - bed_time) / 3600) * 8)
+   total_amount += (((end_shift - bed_time) / 3600) * 8)
   end
   total_amount = total_amount.round(0)
   return total_amount
